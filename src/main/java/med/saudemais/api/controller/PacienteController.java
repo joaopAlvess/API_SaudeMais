11329bade294e318a69f7f0dae2ac8077445191b
matusalem.matusa22@gmail.com <jpfox.alves@gmail.com>
2023-04-06 15:41:13 -0300

    Subindo para o banco de dados

--- src/main/java/med/saudemais/api/controller/PacienteController.java
+++ src/main/java/med/saudemais/api/controller/PacienteController.java
@@ -1,13 +1,9 @@
 package med.saudemais.api.controller;
 
-import med.saudemais.api.paciente.PacienteRepository;
-import med.saudemais.api.paciente.RecordDadosCadastroPaciente;
+import med.saudemais.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -20,6 +16,17 @@ public class PacienteController {
     @PostMapping
     @Transactional
     public void cadastro(@RequestBody RecordDadosCadastroPaciente data) {
-        System.out.println(data);
+        repository.save(new Paciente(data));
     }
+
+    @PutMapping
+    public void atualizar() {
+
+    }
+
+    @DeleteMapping
+    public void deletar() {
+
+    }
+
 }
--- src/main/java/med/saudemais/api/paciente/Paciente.java
+++ src/main/java/med/saudemais/api/paciente/Paciente.java
@@ -1,18 +1,34 @@
 package med.saudemais.api.paciente;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.saudemais.api.endereco.Endereco;
 
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
     private String email;
-
     private String telefone;
-
     private String cpf;
-
+    @Embedded
     private Endereco endereco;
+
+    public Paciente(RecordDadosCadastroPaciente data) {
+        this.nome = data.nome();
+        this.email = data.email();
+        this.cpf = data.cpf();
+        this.telefone = data.telefone();
+        this.endereco = new Endereco(data.endereco());
+    }
 }
