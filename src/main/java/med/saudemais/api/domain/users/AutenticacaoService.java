8aeff6c1047abd92e42dce9fb661ce07ac3e1301
matusalem.matusa22@gmail.com <jpfox.alves@gmail.com>
2023-04-18 14:44:23 -0300

    Subindo para o banco de dados

---
+++ src/main/java/med/saudemais/api/domain/users/AutenticacaoService.java
@@ -0,0 +1,8 @@
+package med.saudemais.api.domain.users;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+}
---
+++ src/main/java/med/saudemais/api/domain/users/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package med.saudemais.api.domain.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
